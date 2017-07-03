a53be5774a0863934c70a1376e4388770d899195
Dani Ruiz <daniel.ruiz@bsc.es>
2017-07-03 16:24:36 +0200

    Added ComputeDotProduct and ComputeWAXPBY optimized codes. They use unrolling, and, for the DotProduct, a version using serial implementation of BLAS is added (need to link against some BLAS library (g.e., ATLAS, OpenBLAS, ARM PL)

--- src/ComputeDotProduct.cpp
+++ src/ComputeDotProduct.cpp
@@ -18,6 +18,19 @@
  HPCG routine
  */
 
+#ifndef HPCG_NO_MPI
+#include <mpi.h>
+#include "mytimer.hpp"
+#endif
+#ifndef HPCG_NO_OPENMP
+#include "omp.h"
+#endif
+#include <cassert>
+
+#ifdef HPCG_USE_OPTIMIZED_BLAS
+#include "armpl.h"
+#endif
+
 #include "ComputeDotProduct.hpp"
 #include "ComputeDotProduct_ref.hpp"
 
@@ -40,8 +53,126 @@
 */
 int ComputeDotProduct(const local_int_t n, const Vector & x, const Vector & y,
     double & result, double & time_allreduce, bool & isOptimized) {
+  assert(x.localLength>=n); // Test vector lengths
+  assert(y.localLength>=n);
 
-  // This line and the next two lines should be removed and your version of ComputeDotProduct should be used.
-  isOptimized = false;
-  return ComputeDotProduct_ref(n, x, y, result, time_allreduce);
+  double local_result = 0.0;
+  double * xv = x.values;
+  double * yv = y.values;
+  /*
+   * Try to unroll to 8, 4 or 2, if not possible, don't do anything
+   */
+  if ( n % 8 == 0 ) {
+	  double local_result1, local_result2, local_result3, local_result4;
+	  double local_result5, local_result6, local_result7, local_result8;
+	  local_result1 = local_result2 = local_result3 = local_result4 = 0.0;
+	  local_result5 = local_result6 = local_result7 = local_result8 = 0.0;
+    if (yv==xv) {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2,local_result3,local_result4,local_result5,local_result6,local_result7,local_result8)
+#endif
+      for (local_int_t i=0; i<n; i+=8) {
+      	local_result1 += xv[i  ]*xv[i  ];
+      	local_result2 += xv[i+1]*xv[i+1];
+      	local_result3 += xv[i+2]*xv[i+2];
+      	local_result4 += xv[i+3]*xv[i+3];
+      	local_result5 += xv[i+4]*xv[i+4];
+      	local_result6 += xv[i+5]*xv[i+5];
+      	local_result7 += xv[i+6]*xv[i+6];
+      	local_result8 += xv[i+7]*xv[i+7];
+      }
+    } else {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2,local_result3,local_result4,local_result5,local_result6,local_result7,local_result8)
+#endif
+      for (local_int_t i=0; i<n; i+=8) {
+      	local_result1 += xv[i  ]*yv[i  ];
+      	local_result2 += xv[i+1]*yv[i+1];
+      	local_result3 += xv[i+2]*yv[i+2];
+      	local_result4 += xv[i+3]*yv[i+3];
+      	local_result5 += xv[i+4]*yv[i+4];
+      	local_result6 += xv[i+5]*yv[i+5];
+      	local_result7 += xv[i+6]*yv[i+6];
+      	local_result8 += xv[i+7]*yv[i+7];
+      }
+    }
+	local_result = local_result1 + local_result2 + local_result3 + local_result4 + local_result5 + local_result6 + local_result7 + local_result8;
+  } else if ( n % 4  == 0 ) {
+	  double local_result1, local_result2, local_result3, local_result4;
+	  local_result1 = local_result2 = local_result3 = local_result4 = 0.0;
+    if (yv==xv) {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2,local_result3,local_result4)
+#endif
+      for (local_int_t i=0; i<n; i+=4) {
+      	local_result1 += xv[i  ]*xv[i  ];
+      	local_result2 += xv[i+1]*xv[i+1];
+      	local_result3 += xv[i+2]*xv[i+2];
+      	local_result4 += xv[i+3]*xv[i+3];
+      }
+    } else {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2,local_result3,local_result4)
+#endif
+      for (local_int_t i=0; i<n; i+=4) {
+      	local_result1 += xv[i  ]*yv[i  ];
+      	local_result2 += xv[i+1]*yv[i+1];
+      	local_result3 += xv[i+2]*yv[i+2];
+      	local_result4 += xv[i+3]*yv[i+3];
+      }
+    }
+	local_result = local_result1 + local_result2 + local_result3 + local_result4;
+  } else if ( n % 2 == 0) {
+	  double local_result1, local_result2;
+	  local_result1 = local_result2 = 0.0;
+    if (yv==xv) {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2)
+#endif
+      for (local_int_t i=0; i<n; i+=2) {
+      	local_result1 += xv[i  ]*xv[i  ];
+      	local_result2 += xv[i+1]*xv[i+1];
+      }
+    } else {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result1,local_result2)
+#endif
+      for (local_int_t i=0; i<n; i+=2) {
+      	local_result1 += xv[i  ]*yv[i  ];
+      	local_result2 += xv[i+1]*yv[i+1];
+      }
+    }
+	local_result = local_result1 + local_result2;
+  } else {
+    if (yv==xv) {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result)
+#endif
+      for (local_int_t i=0; i<n; i++) {
+      	local_result += xv[i]*xv[i];
+      }
+    } else {
+#ifndef HPCG_NO_OPENMP
+    #pragma omp parallel for reduction (+:local_result)
+#endif
+      for (local_int_t i=0; i<n; i++) {
+      	local_result += xv[i]*yv[i];
+      }
+    }
+  }
+
+#ifndef HPCG_NO_MPI
+  // Use MPI's reduce function to collect all partial sums
+  double t0 = mytimer();
+  double global_result = 0.0;
+  MPI_Allreduce(&local_result, &global_result, 1, MPI_DOUBLE, MPI_SUM,
+      MPI_COMM_WORLD);
+  result = global_result;
+  time_allreduce += mytimer() - t0;
+#else
+  time_allreduce += 0.0;
+  result = local_result;
+#endif
+
+  return 0;
 }
--- src/ComputeWAXPBY.cpp
+++ src/ComputeWAXPBY.cpp
@@ -42,7 +42,186 @@
 int ComputeWAXPBY(const local_int_t n, const double alpha, const Vector & x,
     const double beta, const Vector & y, Vector & w, bool & isOptimized) {
 
-  // This line and the next two lines should be removed and your version of ComputeWAXPBY should be used.
-  isOptimized = false;
-  return ComputeWAXPBY_ref(n, alpha, x, beta, y, w);
+  assert(x.localLength>=n); // Test vector lengths
+  assert(y.localLength>=n);
+
+  const double * const xv = x.values;
+  const double * const yv = y.values;
+  double * const wv = w.values;
+  /*
+   * NOTE: Added a new if case where alpha and beta equal to 1. In that case
+   * no multiplications are actually needed.
+   *
+   * Try to unroll to 8, 4 or 2, if not possible, don't do anything
+   */
+  if ( n % 8 == 0) {
+  	if (alpha==1.0 and beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+		for (local_int_t i = 0; i<n; i+=8) {
+			wv[i  ] = xv[i  ] + yv[i  ];
+			wv[i+1] = xv[i+1] + yv[i+1];
+			wv[i+2] = xv[i+2] + yv[i+2];
+			wv[i+3] = xv[i+3] + yv[i+3];
+			wv[i+4] = xv[i+4] + yv[i+4];
+			wv[i+5] = xv[i+5] + yv[i+5];
+			wv[i+6] = xv[i+6] + yv[i+6];
+			wv[i+7] = xv[i+7] + yv[i+7];
+		}
+	  } else if (alpha==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=8) {
+			wv[i  ] = xv[i  ] + beta * yv[i  ];
+			wv[i+1] = xv[i+1] + beta * yv[i+1];
+			wv[i+2] = xv[i+2] + beta * yv[i+2];
+			wv[i+3] = xv[i+3] + beta * yv[i+3];
+			wv[i+4] = xv[i+4] + beta * yv[i+4];
+			wv[i+5] = xv[i+5] + beta * yv[i+5];
+			wv[i+6] = xv[i+6] + beta * yv[i+6];
+			wv[i+7] = xv[i+7] + beta * yv[i+7];
+		}
+ 	 } else if (beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+  	  #pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=8) {
+			wv[i  ] = alpha * xv[i  ] + yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + yv[i+1];
+			wv[i+2] = alpha * xv[i+2] + yv[i+2];
+			wv[i+3] = alpha * xv[i+3] + yv[i+3];
+			wv[i+4] = alpha * xv[i+4] + yv[i+4];
+			wv[i+5] = alpha * xv[i+5] + yv[i+5];
+			wv[i+6] = alpha * xv[i+6] + yv[i+6];
+			wv[i+7] = alpha * xv[i+7] + yv[i+7];
+		}
+ 	 } else  {
+#ifndef HPCG_NO_OPENMP
+ 	   #pragma omp parallel for
+#endif
+		 for (local_int_t i=0; i<n; i+=8) {
+			wv[i  ] = alpha * xv[i  ] + beta * yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + beta * yv[i+1];
+			wv[i+2] = alpha * xv[i+2] + beta * yv[i+2];
+			wv[i+3] = alpha * xv[i+3] + beta * yv[i+3];
+			wv[i+4] = alpha * xv[i+4] + beta * yv[i+4];
+			wv[i+5] = alpha * xv[i+5] + beta * yv[i+5];
+			wv[i+6] = alpha * xv[i+6] + beta * yv[i+6];
+			wv[i+7] = alpha * xv[i+7] + beta * yv[i+7];
+		 }
+ 	 }
+  } else if ( n % 4 == 0) {
+  	if (alpha==1.0 and beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+		for (local_int_t i = 0; i<n; i+=4) {
+			wv[i  ] = xv[i  ] + yv[i  ];
+			wv[i+1] = xv[i+1] + yv[i+1];
+			wv[i+2] = xv[i+2] + yv[i+2];
+			wv[i+3] = xv[i+3] + yv[i+3];
+		}
+	  } else if (alpha==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=4) {
+			wv[i  ] = xv[i  ] + beta * yv[i  ];
+			wv[i+1] = xv[i+1] + beta * yv[i+1];
+			wv[i+2] = xv[i+2] + beta * yv[i+2];
+			wv[i+3] = xv[i+3] + beta * yv[i+3];
+		}
+ 	 } else if (beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+  	  #pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=4) {
+			wv[i  ] = alpha * xv[i  ] + yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + yv[i+1];
+			wv[i+2] = alpha * xv[i+2] + yv[i+2];
+			wv[i+3] = alpha * xv[i+3] + yv[i+3];
+		}
+ 	 } else  {
+#ifndef HPCG_NO_OPENMP
+ 	   #pragma omp parallel for
+#endif
+		 for (local_int_t i=0; i<n; i+=4) {
+			wv[i  ] = alpha * xv[i  ] + beta * yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + beta * yv[i+1];
+			wv[i+2] = alpha * xv[i+2] + beta * yv[i+2];
+			wv[i+3] = alpha * xv[i+3] + beta * yv[i+3];
+		 }
+ 	 }
+  } else if ( n % 2 == 0 ) {
+ 	/*
+ 	 * Unroll these loops
+ 	 */
+  	if (alpha==1.0 and beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+		for (local_int_t i = 0; i<n; i+=2) {
+			wv[i  ] = xv[i  ] + yv[i  ];
+			wv[i+1] = xv[i+1] + yv[i+1];
+		}
+	  } else if (alpha==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=2) {
+			wv[i  ] = xv[i  ] + beta * yv[i  ];
+			wv[i+1] = xv[i+1] + beta * yv[i+1];
+		}
+ 	 } else if (beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+  	  #pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i+=2) {
+			wv[i  ] = alpha * xv[i  ] + yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + yv[i+1];
+		}
+ 	 } else  {
+#ifndef HPCG_NO_OPENMP
+ 	   #pragma omp parallel for
+#endif
+		 for (local_int_t i=0; i<n; i+=2) {
+			wv[i  ] = alpha * xv[i  ] + beta * yv[i  ];
+			wv[i+1] = alpha * xv[i+1] + beta * yv[i+1];
+		 }
+ 	 }
+  } else {
+  	if (alpha==1.0 and beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+		for (local_int_t i = 0; i<n; i++) {
+			wv[i  ] = xv[i  ] + yv[i  ];
+		}
+	  } else if (alpha==1.0) {
+#ifndef HPCG_NO_OPENMP
+		#pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i++) {
+			wv[i  ] = xv[i  ] + beta * yv[i  ];
+		}
+ 	 } else if (beta==1.0) {
+#ifndef HPCG_NO_OPENMP
+  	  #pragma omp parallel for
+#endif
+	    for (local_int_t i=0; i<n; i++) {
+			wv[i  ] = alpha * xv[i  ] + yv[i  ];
+		}
+ 	 } else  {
+#ifndef HPCG_NO_OPENMP
+ 	   #pragma omp parallel for
+#endif
+		 for (local_int_t i=0; i<n; i++) {
+			wv[i  ] = alpha * xv[i  ] + beta * yv[i  ];
+		 }
+ 	 }
+  }
+
+  return 0;
 }
